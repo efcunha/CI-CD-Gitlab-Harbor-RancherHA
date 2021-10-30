9ecfd2308951d4965e4332d0e06d6dc87217c62a
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2021-10-30 19:49:47 -0400

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f /k8s/04-gitlab-data-backup.yml
-kubectl delete -f /k8s/03-ingress.yml
-kubectl delete -f /k8s/02-gitlab.yml
-kubectl delete -f /k8s/01-secrets.yml
-kubectl delete -f /k8s/00-namespace.yml
-
