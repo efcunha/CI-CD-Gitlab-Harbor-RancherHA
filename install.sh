09f26e3606d03971f4cdb2e251cce61f3ec8e2cc
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2021-10-30 19:49:36 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f /k8s/00-namespace.yml
-kubectl apply -f /k8s/01-secrets.yml
-kubectl apply -f /k8s/02-gitlab.yml
-kubectl apply -f /k8s/03-ingress.yml
-kubectl apply -f /k8s/04-gitlab-data-backup.yml
