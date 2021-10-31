2c47e27e82690f1d8ae0dc92c52b0acecb3ec700
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2021-10-31 18:01:23 -0400

    commit

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ kubectl apply -f /k8s/01-secrets.yml
 kubectl apply -f /k8s/02-gitlab.yml
 kubectl apply -f /k8s/03-ingress.yml
 kubectl apply -f /k8s/04-gitlab-data-backup.yml
+kubectl apply -f /k8s/05-gitlab-sa.yaml
 
 ./k8s/autodevops.sh*
