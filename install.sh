ae1c04140286c07d1abe7e545db20e31da4a0c7f
Edson Fernandes Cunha <52961166+efcunha@users.noreply.github.com>
2021-11-05 14:40:37 -0400

    Commit

--- install.sh
+++ install.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+echo "#-----------------------"
+STEP="Criando Namespace Gitlab"
 kubectl apply -f k8s/00-namespace.yml
+checkrc $? ${STEP}
+
+echo "#-----------------------"
+STEP="Fix clusterrolebinding permissive-binding"
+kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts
+checkrc $? ${STEP}
+
+echo "#-----------------------"
+STEP="Instalando o Gitlab"
 kubectl apply -f k8s/01-secrets.yml
 kubectl apply -f k8s/02-account.yml
-kubectl apply -f k8s/03-gitlab.yml
-kubectl apply -f k8s/04-ingress.yml
-kubectl apply -f k8s/05-gitlab-data-backup.yml
+kubectl apply -f k8s/03-service.yml
+kubectl apply -f k8s/04-gitlab-pvc.yml
+kubectl apply -f k8s/05-gitlab.yml
+kubectl apply -f k8s/06-ingress.yml
+kubectl apply -f k8s/07-gitlab-data-backup.yml
+checkrc $? ${STEP}
 
+echo "#-----------------------"
+STEP="Informações para Conectar Gitlab no Kubernetes"
 ./k8s/autodevops.sh*
+checkrc $? ${STEP}
+
+exit 0
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+echo "#-----------------------"
+STEP="Fix clusterrolebinding permissive-binding"
 kubectl delete clusterrolebinding permissive-binding
-kubectl delete -f k8s/05-gitlab-data-backup.yml
-kubectl delete -f k8s/04-ingress.yml
-kubectl delete -f k8s/03-gitlab.yml
+checkrc $? ${STEP}
+
+echo "#-----------------------"
+STEP="Removendo o Gitlab"
+kubectl delete -f k8s/06-ingress.yml
+kubectl delete -f k8s/05-gitlab.yml
+kubectl delete -f k8s/04-gitlab-pvc.yml
+kubectl delete -f k8s/07-gitlab-data-backup.yml
+kubectl delete -f k8s/03-service.yml
 kubectl delete -f k8s/02-account.yml
 kubectl delete -f k8s/01-secrets.yml
+checkrc $? ${STEP}
+
+echo "#-----------------------"
+STEP="Removendo Namespace Gitlab"
 kubectl delete -f k8s/00-namespace.yml
+checkrc $? ${STEP}
+
+exit 0
